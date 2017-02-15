9f693e1bb44d7c5b9f80e227596e9b9fbd53712b
Jason Vigil <vigiljt@gmail.com>
2017-02-14 22:21:26 -0800

    Count Luck complete

--- count_luck/count_luck.go
+++ count_luck/count_luck.go
@@ -17,14 +17,15 @@ func main() {
 	}
 }
 
+// position is a 2d coordinate
 type position struct {
 	x, y int
 }
 
+// treeNode is a node representing a position within the maze
 type treeNode struct {
 	pos position
 	lastPos position
-	depth int
 	children []*treeNode
 }
 
@@ -37,6 +38,7 @@ func runTest(r *bufio.Reader) {
 	start := position{}
 	end := position{}
 
+	// save maze and start/end points
 	for i := 0; i < height; i++ {
 		forest[i] = make([]byte, width)
 		line, _ := r.ReadString('\n')
@@ -54,18 +56,58 @@ func runTest(r *bufio.Reader) {
 		}
 	}
 
+	// save guess as to how many choices it will take to reach end
+	// (aka depth of goal node)
 	var guess int
 	line, _ = r.ReadString('\n')
 	fmt.Sscan(line, &guess)
 
+	// initialize root node as starting position
 	root := new(treeNode)
 	root.pos = start
 	root.lastPos = start
 	root.children = make([]*treeNode, 0)
 
+	// build a tree of important positions
 	buildPositionTree(forest, root)
+
+	// if root has only one child, skip to child
+	if len(root.children) == 1 {
+		root = root.children[0]
+	}
+
+	// perform breadth first search on tree to find goal node
+	toSearch := make([]*treeNode, 0)
+	found := make([]*treeNode, 0)
+
+	for _, node := range root.children {
+		toSearch = append(toSearch, node)
+	}
+
+	depth := 0
+SearchLoop:
+	for len(toSearch) != 0 {
+		depth++
+		for _, node := range toSearch {
+			if node.pos == end {
+				break SearchLoop
+			}
+			found = append(found, node.children...)
+		}
+		toSearch = found
+		found = make([]*treeNode, 0)
+	}
+
+	// output result
+	if depth == guess {
+		fmt.Println("Impressed")
+	} else {
+		fmt.Println("Oops!")
+	}
 }
 
+// buildPositionTree builds a tree of the important positions in a 2d slice representing the maze.
+// Important positions are places where the path forks, dead ends, the start position, and the goal
 func buildPositionTree(forest [][]byte, node *treeNode) {
 	adjacentPositions := getAdjacentPositions(forest, node.pos, node.lastPos)
 	for _, pos := range adjacentPositions {
@@ -82,6 +124,8 @@ func buildPositionTree(forest [][]byte, node *treeNode) {
 	}
 }
 
+// getNextPosition returns the next position of interest after currentPos. Positions of interest
+// are either places where the path forks, dead ends, or the goal position
 func getNextPosition(forest [][]byte, currentPos, lastPos position) (position, position) {
 	for {
 		adjacentPositions := getAdjacentPositions(forest, currentPos, lastPos)
@@ -101,6 +145,8 @@ func getNextPosition(forest [][]byte, currentPos, lastPos position) (position, p
 	return currentPos, lastPos
 }
 
+// getAdjacentPositions returns a slice of possible positions to move to after currentPos, not
+// counting lastPos (to prevent moving backwards)
 func getAdjacentPositions(forest [][]byte, currentPos, lastPos position) []position {
 	adjacentPositions := make([]position, 0)
 
@@ -125,6 +171,7 @@ func getAdjacentPositions(forest [][]byte, currentPos, lastPos position) []posit
 	return adjacentPositions
 }
 
+// isValidPosition returns true if pos is a valid position to move within the forest (. or *)
 func isValidPosition(forest [][]byte, pos position) bool {
 	if pos.x >= 0 && pos.x < len(forest[0]) && pos.y >= 0 && pos.y < len(forest) {
 		if forest[pos.y][pos.x] == '.' || forest[pos.y][pos.x] == '*' {
